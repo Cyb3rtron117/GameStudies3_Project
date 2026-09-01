fa50049ba32efb36217c7542d864ac5ac92c9339
Cyb3rtron <dian.ehrke@gmail.com>
2026-09-01 14:38:10 +0200

    Barriers in progress

---
+++ GS3_Game/Assets/Scripts/Game/GoalBarrier.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class GoalBarrier : MonoBehaviour
+{
+    private Animator anim;
+    public Team whichGoal;
+    private float waitTime = 1f;
+
+    private void Start()
+    {
+        if(anim == null)
+        {
+            Debug.LogWarning("Animator not assigned!");
+        }
+    }
+    IEnumerator Opengoal(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        anim.SetBool("Open", true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Tank"))
+        {
+            Team _team = other.gameObject.GetComponent<Tank_Manager>()._team;
+            if(_team != whichGoal)
+            {
+                StartCoroutine(Opengoal(waitTime));
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Tank"))
+        {
+            Team _team = other.gameObject.GetComponent<Tank_Manager>()._team;
+            if (_team != whichGoal)
+            {
+                StopCoroutine(Opengoal(waitTime));
+                anim.SetBool("Open", false);
+            }
+        }
+    }
+}
